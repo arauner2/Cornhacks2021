1050297ef274f07a2d91a33e55833f5739ecf79f
Emma Clausen <eclausen667@gmail.com>
2021-01-30 14:31:12 -0600

    adding more customizing

--- popup.js
+++ popup.js
@@ -13,3 +13,19 @@ changeColor.onclick = function(element) {
       {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
+
+let changeFont = document.getElementById('changeFont');
+
+chrome.storage.sync.get('font', function(data) {
+  changeFont.style.fontFamily = data.font;
+  changeFont.setAttribute('value', data.font);
+});
+
+changeFont.onclick = function(element) {
+  let font = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: 'document.body.style.fontFamily = "' + font + '";'});
+  });
+};
