ab5ffb11a544680a3282e314979146ac210d38a5
shengjlim <sheng-jie.lim@huskers.unl.edu>
2021-01-30 14:31:00 -0600

    Added code to get assignments

--- popup.js
+++ popup.js
@@ -1,15 +1,26 @@
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+// window.onload = function(){
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
+// }
+console.log("yooooo before function");
+function getAssignments() {
+  const assignments = document.getElementsByClassName("Xzp3fc")[0].getElementsByTagNmae('li');
+  
+  
 };
+
+//getAssignments();x
+window.addEventListener("load", getAssignments);
