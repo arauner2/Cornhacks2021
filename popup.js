3abd1260cb9f19999845cb6e7f13824ee2bb5173
Ben Stuart <ben@chartmetric.io>
2021-01-30 19:10:51 -0600

    stuff

--- popup.js
+++ popup.js
@@ -1,12 +1,11 @@
 // Background Color
 let changeColor = document.getElementById('changeColor');
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
+changeColor.addEventListener('change', function(element) {
   console.log("BG COLOR");
   let color = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -14,17 +13,15 @@ changeColor.onclick = function(element) {
       tabs[0].id,
       {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
-};
+}, false);
 
 // Font Family
 let changeFont = document.getElementById('changeFont');
-
-chrome.storage.sync.get('font', function(data) {
-  changeFont.style.fontFamily = data.font;
-  changeFont.setAttribute('value', data.font);
-});
-
-changeFont.onclick = function(element) {
+// chrome.storage.sync.get('font', function(data) {
+//   changeFont.style.fontFamily = data.font;
+//   changeFont.setAttribute('value', data.font);
+// });
+changeFont.addEventListener('change', function(element) {
   console.log("FONT");
   let font = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -32,80 +29,77 @@ changeFont.onclick = function(element) {
       tabs[0].id,
       {code: 'document.body.style.fontFamily = "' + font + '";'});
   });
-};
+}, false);
 
 //Background Image
 
 let changeImage = document.getElementById('changeImage');
 
-chrome.storage.sync.get('font', function(data) {
-  changeImage.style.backgroundImage = data.image;
-  changeImage.setAttribute('value', data.image);
-});
+// chrome.storage.sync.get('font', function(data) {
+//   changeImage.style.backgroundImage = data.image;
+//   changeImage.setAttribute('value', data.image);
+// });
 
-changeColor.onclick = function(element) {
+changeImage.addEventListener('change', function(element) {
   console.log("IMAGE");
   let image = element.target.value;
+  let code = 'document.body.style.backgroundImage = "' + "url('" + image + "')\";"
+  console.log(code);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
-      {code: 'document.body.style.backgroundImage = "' + image + '";'});
+      {code: code});
   });
-};
+}, false);
 
-var colors = {
-  '#FFFFFF': 'Default',
-  '#000080': 'Blue',
-  '#00FF00': 'Green', 
-  '#DFFF00': 'Yellow',
-  '#FF0000': 'Red',
-  '#FF00FF': 'Fuchsia',
-  '#C0C0C0': 'Maroon',
-  '#00FFFF': 'Aqua', 
-  '#000080': 'Navy', 
-  '#808000': 'Olive',
-  '#FF7F50': 'Peach'
-}
 
-var fonts = {
-  'Helvetica': 'Helvetica',
-  'Calibri': 'Calibri',
-  'Arial': 'Arial',
-  'Georgia': 'Georgia',
-  'Garamond': 'Garamond',
-  'Verdana': 'Verdana',
-  'Tahoma': 'Tahoma',
-  'Times New Roman': 'Times New Roman',
-  'Courier New': 'Courier New'
-}
+var colors = [
+  ['#FFFFFF', 'Default'],
+  ['#000080', 'Blue'],
+  ['#00FF00', 'Green'], 
+  ['#DFFF00', 'Yellow'],
+  ['#FF0000', 'Red'],
+  ['#FF00FF', 'Fuchsia'],
+  ['#C0C0C0', 'Maroon'],
+  ['#00FFFF', 'Aqua'], 
+  ['#000080', 'Navy'], 
+  ['#808000', 'Olive'],
+  ['#FF7F50', 'Peach']
+]
 
-var images = {
-  'alien': 'images/alien.png',
-  'basketball': 'images/basketball.png',
-  'car': 'images/car.png',
-  'dinosaur': 'images/dinosaur.png',
-  'duck': 'images/duck.png',
-  'guitar': 'images/guitar.png',
-  'pokemon': 'images/pokemon.png',
-  'skateboard': 'images/skateboard.png',
-  'tree': 'images/tree.png',
-  'unicorn': 'images/unicorn.png'
-}
+var fonts = [
+  ['Helvetica', 'Helvetica'],
+  ['Calibri', 'Calibri'],
+  ['Arial', 'Arial'],
+  ['Georgia', 'Georgia'],
+  ['Garamond', 'Garamond'],
+  ['Verdana', 'Verdana'],
+  ['Tahoma', 'Tahoma'],
+  ['Times New Roman', 'Times New Roman'],
+  ['Courier New', 'Courier New']
+  ]
+var images = [
+  ['Alien', 'images/alien.png'],
+  ['Basketball', 'images/basketball.png'],
+  ['Car', 'images/car.png'],
+  ['Dinosaur', 'images/dinosaur.png'],
+  ['Duck', 'images/duck.png'],
+  ['Guitar', 'images/guitar.png'],
+  ['Pokemon', 'images/pokemon.png'],
+  ['Skateboard', 'images/skateboard.png'],
+  ['Tree', 'images/tree.png'],
+  ['Unicorn', 'images/unicorn.png']
+]
 
 var assignments;
 let level = 6;
 
-let select = document.getElementById("changeColor");
-for (i in colors[0,level]){
-  select.options[select.options.length] = new Option(colors[i], i);
+for (let i = 0; i < level; i++){
+  //Color
+  changeColor.options[changeColor.options.length] = new Option(colors[i][1], colors[i][0]);
+  //Font
+  changeFont.options[changeFont.options.length] = new Option(fonts[i][1], fonts[i][0]);
+  //Image
+  changeImage.options[changeImage.options.length] = new Option(images[i][0], images[i][1]);
 }
-
-select = document.getElementById("changeFont");
-for (i in fonts[0,level]){
-  select.options[select.options.length] = new Option(fonts[i], i);
-}
-
-select = document.getElementById("changeImage");
-for (i in images[0,level]){
-  select.options[select.options.length] = new Option(images[i], i);
-}
+console.log(changeColor, changeFont, changeImage);
